60f653907de9ffcf676a519ab89d3f11479c72ff
Mike Wadsten <mikewadsten@gmail.com>
2014-02-12 08:56:30 -0600

    Update fish cd alias to skip ls call with '--help'. '-h' not working...?

--- .customizations/.config/fish/functions/cd.fish
+++ .customizations/.config/fish/functions/cd.fish
@@ -1,8 +1,12 @@
-function cd
-    if not set -q $argv[1]
-        builtin cd $HOME
+function cd --description 'Alias for "cd" function, running ls afterwards.'
+    if count $argv >/dev/null
+        builtin cd $argv[1]
+        if [ "$argv[1]" = "-h" -o "$argv[1]" = "--help" ]
+            # Return now, because we were just asking for help...
+            return
+        end
     else
-        builtin cd $argv
+        builtin cd $HOME
     end
     ls
 end
